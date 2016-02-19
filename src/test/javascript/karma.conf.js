b1f49d4f0891e9d74228dce7e7a6766b60a82392
unknown <PRBFGP@TLDAM4608.lda>
2016-02-19 13:41:33 +0100

    Reinstall Phantom JS

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -68,7 +68,7 @@ module.exports = function (config) {
         },
 
         // web server port
-        port: 9877,
+        port: 9876,
 
         // level of logging
         // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
